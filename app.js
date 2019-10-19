8c2b4610c974523f3820e96818ab2e931ac1f0a7
ahmed habeeb <az73system@gmail.com>
2019-10-19 10:22:53 +0200

    add route and nodemon

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express= require("express");
 
 const app= express();
 
+
+//index route
+
+app.get('/', (req, res) => {
+  res.send("index page");
+});
+
+//about route
+
+app.get('/about', (req, res) => {
+  res.send("about");
+});
+
+
+
+
+
 const PORT=3000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
