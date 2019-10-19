43eeeb15a335b87bb9969ca6b0a6e1f2fb692402
ahmed habeeb <az73system@gmail.com>
2019-10-19 11:56:22 +0200

    I add handlebars temp and create index and about page

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express= require("express");
+const exphbs  = require('express-handlebars');
+
 
 const app= express();
+
+
+
+//handlebar 
+
+app.engine('handlebars', exphbs({
+  defaultLayout:"main"
+}));
+app.set('view engine', 'handlebars'); 
 
 
 //index route
 
 app.get('/', (req, res) => {
-  res.send("index page");
+  res.render("index");
 });
 
 //about route
